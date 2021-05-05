381105037165f725d33f860be9950612edc70140
krcook1980 <kellyrcook1980@gmail.com>
2021-05-05 14:52:28 -0600

    fix nav bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import {Navbar, Nav, NavLink } from 'react-bootstrap';
 
 function NavComponent() {
     return (
@@ -7,9 +7,9 @@ function NavComponent() {
             <Nav className="m-auto">
             <Navbar.Toggle className="navbar-dark" aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav  text-center">
-                    <NavDropdown.Item href="#Home" className="text-white mr-5 pr-5">Home</NavDropdown.Item>
-                    <NavDropdown.Item href="#About" className="text-white ml-5 mr-5 pr-5 pl-5">About</NavDropdown.Item>
-                    <NavDropdown.Item href="#Portfolio" className="text-white ml-5 pl-5">Portfolio</NavDropdown.Item>
+                    <NavLink href="/" className="text-white mr-5 pr-5 nav-item">Home</NavLink>
+                    <NavLink href="#About" className="text-white ml-5 mr-5 pr-5 pl-5 nav-item">About</NavLink>
+                    <NavLink href="#Portfolio" className="text-white ml-5 pl-5 nav-item">Portfolio</NavLink>
                 </Navbar.Collapse>
             </Nav>
 
