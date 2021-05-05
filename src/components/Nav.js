98f198609277e3f67d4ff6be907d4efd5937cd2b
krcook1980 <kellyrcook1980@gmail.com>
2021-05-05 14:56:19 -0600

    broke home button

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,7 +7,7 @@ function NavComponent() {
             <Nav className="m-auto">
             <Navbar.Toggle className="navbar-dark" aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav  text-center">
-                    <NavLink href="/" className="text-white mr-5 pr-5 nav-item">Home</NavLink>
+                    <NavLink href="#Home" className="text-white mr-5 pr-5 nav-item">Home</NavLink>
                     <NavLink href="#About" className="text-white ml-5 mr-5 pr-5 pl-5 nav-item">About</NavLink>
                     <NavLink href="#Portfolio" className="text-white ml-5 pl-5 nav-item">Portfolio</NavLink>
                 </Navbar.Collapse>
