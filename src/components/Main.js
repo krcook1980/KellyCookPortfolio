3135ca7aeee07752cbfc311154aee4bffc370cbe
krcook1980 <kellyrcook1980@gmail.com>
2021-06-09 13:00:22 -0600

    update about with link to certificate of completion

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,7 +7,7 @@ function Main() {
                 <h1 className="h1">All About Kelly Cook</h1>
                 <p className="about">
                     <img className="profilePic rounded" src="https://github.com/krcook1980/FullStackPortfolio/blob/main/assets/IMG_7512.jpg?raw=true" alt="Kelly Cook" />
-            I am a fullstack developer. I enjoy both designing websites and working with databases, perhaps one day I will choose a favorite, but not today. I have completed a Fullstack Developer course through the University of Utah with an A+ grade average, and am excited to receive my official Certificate of Completion soon. All of my group projects have garnered class voted awards. I am fully capable of working with HTML, CSS, JS, React, MySql, MongoDB, Node, Express, and more. I have started to study Python, and intend to continue my studies into Java, React Native and Amazon Web Services.
+            I am a fullstack developer. I enjoy both designing websites and working with databases, perhaps one day I will choose a favorite, but not today. I have completed a Fullstack Developer course through the University of Utah with an A+ grade average, and obtained my <a href="https://www.credly.com/badges/4116dd08-a86a-49bb-a162-5b6569811c60" className="text-info" target="blank">Certificate of Completion.</a> All of my group projects have garnered class voted awards. I am fully capable of working with HTML, CSS, JS, React, MySql, MongoDB, Node, Express, and more. I have started to study Python, and intend to continue my studies into Java, React Native and Amazon Web Services.
             </p>
                 <p className="about">
                     I am not just committed to succeed, but also to genuinely enjoy the journey of learning and growing my skills as a developer. I am able to utilize all the skills I learned in my past lives as a Project Manager, Accountant, School Finance Specialist, and even the hospitality industry, to better my understanding of client needs to exceed all expectations.
@@ -20,6 +20,8 @@ function Main() {
                     peers also have every opportunity for success.  I am able to take prior experience and apply it to new situations. I am eager to continually grow and learn. I strive daily to better myself in every way.
             </p>
             </div>
+           
+            
 
         </div>
     )
